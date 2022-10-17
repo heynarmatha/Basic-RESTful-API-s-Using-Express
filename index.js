a073574c6e3fc1456953a0b5dace4290664f965e
Narmatha <narmatha.s@fibonalabs.com>
2022-10-17 22:10:08 +0530

    basic code for http

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("First GET API");
+});
+
+app.get("/api/v1", (req, res) => {
+  res.send("Second GET API");
+});
+
+app.listen(3000, () => console.log("listening to 3000 port"));
